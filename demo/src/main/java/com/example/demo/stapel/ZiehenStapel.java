5d46f6d29fe155bcc50a3f3be2c17f65f65bd25f
Florian Kenner <w21119@lehre.dhbw-stuttgart.de>
2022-05-11 20:49:26 +0200

    some first attributes and functions of ZiehenStapel.java

--- demo/src/main/java/com/example/demo/stapel/ZiehenStapel.java
+++ demo/src/main/java/com/example/demo/stapel/ZiehenStapel.java
@@ -1,4 +1,21 @@
 package com.example.demo.stapel;
 
+import java.util.Stack;
+
 public class ZiehenStapel {
+    private Stack<String> kartenStack = new Stack<>();
+    public int laenge;
+
+    public String nehmen(){
+        return kartenStack.pop();
+    }
+
+
+    public void generieren(){
+        kartenStack.push("test");
+
+
+    }
 }
+
+
