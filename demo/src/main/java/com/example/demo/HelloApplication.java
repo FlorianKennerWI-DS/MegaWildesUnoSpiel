7ef2a277cb4706c350f961925a6b796a335a3bf4
Malena Knoch <malena@knoch-online.de>
2022-05-15 11:37:43 +0200

    added background

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,7 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 600, 420);
+        scene.getStylesheets().addAll(this.getClass().getResource("hello-view-style.css").toExternalForm());
         stage.setTitle("Welcome to UNO!");
         stage.setScene(scene);
         stage.show();
