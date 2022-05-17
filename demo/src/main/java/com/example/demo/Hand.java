d63c61bd712f8bd954fab8129f4c96201fd50f67
Malena Knoch <malena@knoch-online.de>
2022-05-17 18:53:11 +0200

    saving current progress

---
+++ demo/src/main/java/com/example/demo/Hand.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+
+/**
+ * A typical player hand
+ *
+ * @author Almas
+ */
+public class Hand {
+
+    private ObservableList<Node> cards;
+    private SimpleIntegerProperty value = new SimpleIntegerProperty(0);
+
+    private int aces = 0;
+
+    public Hand(ObservableList<Node> cards) {
+        this.cards = cards;
+    }
+
+    public void takeCard(Card card) {
+        cards.add(card);
+    }
+
+    public void reset() {
+        cards.clear();
+        value.set(0);
+        aces = 0;
+    }
+
+    public SimpleIntegerProperty valueProperty() {
+        return value;
+    }
+}
+
