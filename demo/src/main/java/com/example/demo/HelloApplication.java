0c26c2575ead2da0095998b78e5ccbc206fd8497
Malena Knoch <malena@knoch-online.de>
2022-05-18 09:27:21 +0200

    switching branches

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -4,11 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private Label moderateLabel;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -17,7 +19,12 @@ public class HelloApplication extends Application {
         scene.getStylesheets().addAll(this.getClass().getResource("hello-view-style.css").toExternalForm());
         stage.setTitle("Welcome to UNO!");
         stage.setScene(scene);
+        setText();
         stage.show();
+    }
+
+    private void setText() {
+        moderateLabel.setText("Hallo! :)");
     }
 
     public static void main(String[] args) {
