b6a68cd7af55a04deb2007ac7433c1bdf449f58b
Florian Kenner <Florian.Kenner@hpe.com>
2022-05-19 09:20:19 +0200

    WIP: ZiehenStapel

--- demo/src/main/java/com/example/demo/stapel/ZiehenStapel.java
+++ demo/src/main/java/com/example/demo/stapel/ZiehenStapel.java
@@ -1,4 +1,36 @@
 package com.example.demo.stapel;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class ZiehenStapel {
+    private Stack<String> kartenStack = new Stack<>();
+    public int laenge;
+
+    public String nehmen(){
+        return kartenStack.pop();
+    }
+    public static Stack<String> generieren(){
+        ArrayList<String> alleKarten = new ArrayList<String>(){};
+        alleKarten.add("a");
+        alleKarten.add("b");
+
+        int kartenAnzahl = alleKarten.size();
+        Stack<String> gemischterStapel = new Stack<>();
+
+        for (int i = 0; i < kartenAnzahl; i ++){
+            int index = ThreadLocalRandom.current().nextInt(0, alleKarten.size()) ;
+            gemischterStapel.push(alleKarten.get(index));
+            alleKarten.remove(index);
+        }
+        return gemischterStapel;
+
+
+
+
+
+    }
 }
+
+
