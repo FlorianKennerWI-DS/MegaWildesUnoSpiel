aa3494c61d507833a03825035b8bade76088f0dc
Annika Bender <wi21118@lehre.dhbw-stuttgart.de>
2022-05-19 18:07:03 +0200

    Klasse Spieler: Handkarten als ArrayLists in ArrayList

--- demo/src/main/java/com/example/demo/spieler/Spieler.java
+++ demo/src/main/java/com/example/demo/spieler/Spieler.java
@@ -1,16 +1,45 @@
 package com.example.demo.spieler;
 
+import java.lang.reflect.UndeclaredThrowableException;
 import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Spieler {
-    //Handkarten aus Klasse Karten?
-    ArrayList<Karte> handkarten = new ArrayList<Karte>(); // zu Anfang 7 aber theoretisch unbegrenzt
-
-    public void ziehen(Karte karte) {
-        handkarten.add(karte);   // Methode nehmen in Klammern aufrufen
+    //Handkarten aus Klasse Karten
+    ArrayList<Karte> rot = new ArrayList<Karte>();
+    ArrayList<Karte> blau = new ArrayList<Karte>();
+    ArrayList<Karte> gelb = new ArrayList<Karte>();
+    ArrayList<Karte> gruen = new ArrayList<Karte>();
+    ArrayList<Karte> schwarz = new ArrayList<Karte>();
+    ArrayList<ArrayList<Karte>> handkarten = new ArrayList<ArrayList<Karte>>();
+    handkarten.add(schwarz);
+    public void ziehen(Karte karte) {   //Zahlen mit Sortieralgorithmen
+        if (karte.getFarbe().equals("Rot")) {
+            handkarten.get(rot).add(karte);
+        }
+        if (karte.getFarbe().equals("Blau")) {
+            blau.add(karte.getZahl());
+            handkarten.add(blau);
+        }
+        if (karte.getFarbe().equals("Gelb")) {
+            gelb.add(karte.getZahl());
+            handkarten.add(gelb);
+        }
+        if (karte.getFarbe().equals("Gruen")) {
+            gruen.add(karte.getZahl());
+            handkarten.add(gruen);
+        }
+        if (karte.getFarbe().equals("Schwarz")) {
+            schwarz.add(karte.getZahl());
+            handkarten.add(schwarz);
+        }
     }
 
     public void ablegen(Karte karte) {
-        handkarten.remove(karte);    // Stelle oder Inhalt in Klammern -> Methode aufrufen
+        //handkarten.remove(karte.getFarbe(), karte);
+        if (karte.getFarbe().equals("Rot")) {
+            handkarten.get(rot).remove(karte);
+        }
     }
 }
