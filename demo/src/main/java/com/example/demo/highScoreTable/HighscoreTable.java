9df45fd75bebd0688495c51c2c13eac6c4373715
Florian Kenner <Florian.Kenner@hpe.com>
2022-05-20 21:23:17 +0200

    add HighscoreTable.berechneDurchschnittsPosition

--- demo/src/main/java/com/example/demo/highScoreTable/HighscoreTable.java
+++ demo/src/main/java/com/example/demo/highScoreTable/HighscoreTable.java
@@ -1,31 +1,25 @@
 package com.example.demo.highScoreTable;
 
-import com.example.demo.spieler.Spieler;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class HighscoreTable {
     private static HashMap<String, ArrayList<Integer>> alleSpieler = new HashMap<String, ArrayList<Integer>>();
+    private static HashMap<String, Integer> alleDurschnittsPositionen = new HashMap<String, Integer>();
 
     public static void einlesen() {
         String zeile = null;
-        String spielerName;
-        int position;
         Path meineDatei = Paths.get("./GameData.csv");
         try {
             // check if file with highscoredata exists, if not create it and write first line
             if (!Files.exists(meineDatei)){
                 Files.createFile(meineDatei);
                 BufferedWriter initialerWriter = Files.newBufferedWriter(meineDatei);
-                initialerWriter.write("Name, Position");
+                initialerWriter.write("Name,Position");
                 initialerWriter.close();
             }
             BufferedReader meinReader = Files.newBufferedReader(meineDatei);
@@ -34,8 +28,8 @@ public class HighscoreTable {
 
             while (zeile != null){
                 StringTokenizer zeilenToken = new StringTokenizer(zeile, ",");
-                spielerName = zeilenToken.nextToken();
-                position = Integer.parseInt(zeilenToken.nextToken());
+                String spielerName = zeilenToken.nextToken();
+                int position = Integer.parseInt(zeilenToken.nextToken());
 
                 // check if name already exists in alleSpieler
                 if (alleSpieler.containsKey(spielerName)){
@@ -44,16 +38,31 @@ public class HighscoreTable {
 
                 // else add name as key and position as value
                 else {
-                    alleSpieler.put(spielerName, new ArrayList<>(position));
+                    alleSpieler.put(spielerName, new ArrayList<>());
+                    alleSpieler.get(spielerName).add(position);
                 }
+                zeile = meinReader.readLine();
             }
+            System.out.println(alleSpieler);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        // TODO: do something with this information
+
     }
 
+    public static void berechneDurchschnittsPosition(){
+        for (var entry : alleSpieler.entrySet()){
+            int positionsSumme = 0;
+            String name = entry.getKey();
+            for (int aktuellerPlatz : entry.getValue()){
+                positionsSumme = positionsSumme + aktuellerPlatz;
+            }
+            int eintelneDurchschnittsPosition = positionsSumme / entry.getValue().size();
+            alleDurschnittsPositionen.put(name, eintelneDurchschnittsPosition);
+        }
+    }
+/*
     public void spielAbspeichern(ArrayList<Spieler> spielende){
         
-    }
+    }*/
 }
