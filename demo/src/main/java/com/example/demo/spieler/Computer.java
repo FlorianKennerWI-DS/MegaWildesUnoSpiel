150dc9f283f1b925265433fb700d25d2ca4237dd
Florian Kenner <Florian.Kenner@hpe.com>
2022-05-21 16:31:04 +0200

    WIP: add Computer.karteFinden

--- demo/src/main/java/com/example/demo/spieler/Computer.java
+++ demo/src/main/java/com/example/demo/spieler/Computer.java
@@ -1,13 +1,33 @@
 package com.example.demo.spieler;
+import com.example.demo.Karten.Karte;
 import com.example.demo.stapel.AblegeStapel;
 
 public class Computer extends Spieler {
 
-    public void karteFinden() {
+
+    public Computer(int i) {
+        super("com" + Integer.toString(i));
+    }
+
+    public Karte karteFinden(Karte obersteKarte) throws Exception {
+        if (!nochNeuereHandkarten.get(obersteKarte.getFarbe()).containsKey(obersteKarte.getZahl())) {
+            throw new Exception();
+        }
+        if (nochNeuereHandkarten.get(obersteKarte.getZahl()).containsKey(obersteKarte.getFarbe()) && nochNeuereHandkarten.get(obersteKarte.getFarbe()).get(obersteKarte.getZahl()).size() == 0) {
+            throw new Exception();
+        }
+        return ablegen(nochNeuereHandkarten.get(obersteKarte.getFarbe()).get(obersteKarte.getZahl()).get(0));
         // alle durchgehen
         //if(AblegeStapel.getObersteKarte().getFarbe().equals(handkarten.get(0))) {
         //    ablegen(handkarten.get()); }
-         // macht es hier mehr Sinn die Farbe und dann evtl Zahl der obersten Karte abzufragen oder
-            // die Karte mit der kompatibleFarbe und kompatibleZahl aus AblegeStapel zu vergleichen?
+        // macht es hier mehr Sinn die Farbe und dann evtl Zahl der obersten Karte abzufragen oder
+        // die Karte mit der kompatibleFarbe und kompatibleZahl aus AblegeStapel zu vergleichen?
     }
 }
+    /**
+    public Karte ablegen() throws Exception{
+        Karte abgelegteKarte = null;
+        if (handkarten.get())
+    }
+}
+*/
