de16be9c6c5315fb0ee9b6734be6e7705a779290
KimmyDoubleYou <kim.02@gmx.de>
2022-05-21 14:58:31 +0200

    Exception in Karte nehmen (zweiter Versuch)

--- demo/src/main/java/com/example/demo/stapel/ZiehenStapel.java
+++ demo/src/main/java/com/example/demo/stapel/ZiehenStapel.java
@@ -5,20 +5,19 @@ import com.example.demo.Karten.Karte;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.ArrayList;
 import java.util.Stack;
-import java.text.ParseException;
 
 public class ZiehenStapel {
     private Stack<Karte> kartenStack = new Stack<>();
     public int laenge;
 
     public Karte nehmen(){
-        if(laenge ==0){
-            throw new IllegalArgumentException("Stapel leer");
-        }
-        else
-            return kartenStack.pop();
 
+            if (laenge == 0) {
+                throw new IllegalArgumentException("Stapel leer");
+            } else
+                return kartenStack.pop();
         }
+
 
 
     public static Stack<Karte> generieren(){
