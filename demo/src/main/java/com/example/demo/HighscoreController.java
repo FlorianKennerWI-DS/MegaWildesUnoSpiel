e9b58edba3d26505a974ce5951cd6dc71334ff86
Florian Kenner <w21119@lehre.dhbw-stuttgart.de>
2022-05-23 18:52:45 +0200

    add zurueckButton to HighscoreController.java to return to starting Scene

--- demo/src/main/java/com/example/demo/HighscoreController.java
+++ demo/src/main/java/com/example/demo/HighscoreController.java
@@ -1,16 +1,29 @@
 package com.example.demo;
 
 import com.example.demo.highScoreTable.HighscoreTable;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class HighscoreController implements Initializable {
+
+    private Stage stage;
+    private Scene scene;
     @FXML
     public ListView highScoreListView;
+    public Button zurueckButton;
 
     public void updateHighScoreListView() {
         HighscoreTable.einlesen();
@@ -23,5 +36,14 @@ public class HighscoreController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         updateHighScoreListView();
+    }
+
+    public void backToStartScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("hello-view.fxml"))));
+        stage =(Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
