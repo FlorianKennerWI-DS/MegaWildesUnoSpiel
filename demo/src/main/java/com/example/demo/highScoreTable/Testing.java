96df6b44e73cb0f4c5db7cfa6d1cc5798d802dcf
Malena Knoch <malena@knoch-online.de>
2022-05-24 18:09:26 +0200

    my progress

--- demo/src/main/java/com/example/demo/highScoreTable/Testing.java
+++ demo/src/main/java/com/example/demo/highScoreTable/Testing.java
@@ -1,11 +1,5 @@
 package com.example.demo.highScoreTable;
 
-import com.example.demo.HighscoreController;
-import com.example.demo.karten.Karte;
-import com.example.demo.spieler.Computer;
-import com.example.demo.spieler.Spieler;
-
-import java.util.ArrayList;
 
 public class Testing {
     public static void main(String[] args) {
